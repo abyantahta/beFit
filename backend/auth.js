5efc8035fe7bad19b4678a6123ad1905cabb6f05
niamillaa <79699576+niamillaa@users.noreply.github.com>
2023-06-11 15:54:07 +0700

    Add files via upload

---
+++ backend/auth.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs')
+const newClient = require('./connection')
+const { resolve } = require('path')
+const { reject } = require('bluebird')
+const register=(data)=>{
+    return new Promise(async(resolve, reject)=>{
+        const hashedPassword = await bcrypt.hash(data.password, 8)
+        const client = newClient()
+        client.connect()
+        client.query(`INSERT into public.user(username, email, password, phone_number, name)
+                      values('${data.username}','${data.email}','${hashedPassword}','${data.phone_number}', '${data.name}')`, (err)=>{
+                        if(err){
+                            reject(err.message)
+                        }
+                        resolve({message : "Register success"})
+                        client.end()
+                    })
+    })
+}
+
+const login=(data)=>{
+    return new Promise(async(resolve, reject)=>{
+        const client = newClient()
+        client.connect()
+        client.query(`SELECT * from public.user where username = '${data.username}'`, async (err, result)=>{
+            if(err){
+                reject(err.message)
+            }
+            if(!result.rows[0]){
+                reject({message : "Data Tidak Ditemukan"})
+            }
+            const match = await bcrypt.compareSync(data.password, result.rows[0].password)
+            if(!match){
+                reject({ message : "Password Salah"})
+            }
+            resolve({message : "Login success", data : result.rows[0]})
+            client.end()
+        })
+    })
+}
+
+module.exports = {
+    register,
+    login
+}
---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express=require('express');
+const { register, login } = require('./auth');
+const app=express();
+
+app.use(express.json());
+app.get("/",(req, res)=>{
+    res.json({message:"Berhasil Register"})
+})
+app.post("/register", (req, res)=>{
+    try {
+        if(!(req.body.username && req.body.email && req.body.password && req.body.phone_number && req.body.name)){
+            res.status(400).json({message : "Data Belum Lengkap"})
+        }
+        if(!req.body.username.length>=3){
+            res.status(400).json({message : "Username Tidak Mencukupi"})
+        }
+        if(!req.body.password.length>=6){
+            res.status(400).json({message : "Password Minimal 6 Karakter"})
+        }
+        return register(req.body).then((result)=>{
+            res.json(result)
+        })
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+})
+
+app.post("/login", (req,res) => {
+    try {
+        if(!(req.body.username && req.body.password)){
+            res.status(400).json({message:"Data Belum Lengkap"})
+        }
+        return login(req.body).then((result) => {
+            res.json(result)
+        })
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+})
+
+app.listen(8080, ()=>{
+    console.log("apps jalan")
+})
