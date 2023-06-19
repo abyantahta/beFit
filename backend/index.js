ceb52339f7dadcc899df997cfd5723f6c72132fc
Abyantahta <abyantahta2002@gmail.com>
2023-06-19 19:32:19 +0700

    finishing

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import connectDB from './config/db.js'
 import router from './routes/router.js';
 const app = express();
-
-connectDB()
 
 app.use(cors());
 app.use(express.json())
